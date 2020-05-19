3ff0f2521285680b62fb267c5c5c058f0ad367d6
karn21 <karn212000@gmail.com>
2020-05-19 12:01:32 +0530

    fixed index

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -39,7 +39,6 @@ export const login = (username, password) => (dispatch) => {
     },
   };
   const body = JSON.stringify({ username, password });
-  console.log(body);
   axios
     .post("api/auth/login", body, config)
     .then((res) => {
